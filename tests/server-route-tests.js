c40f66ce393e873f028034abbbb3430bf304ffdb
Daniel King <daniel.oliver.king@gmail.com>
2016-09-15 11:03:31 -0700

    Set up server testing skeleton

---
+++ tests/server-route-tests.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+
+const PORT = process.env.PORT || 3000;
+const HOST = `http://localhost:${PORT}`
+
+require('../server.js');
+
+describe('Server route', function() {
+  describe('GET /', function() {
+    it('should respond with html', function(done) {
+
+    });
+
+    it('should respond with a 200 status', function(done) {
+
+    });
+  });
+
+  describe('GET /user/:username/:password', function() {
+    it('should respond with nothing when username and password are not found in the database', function(done) {
+
+    });
+
+    it('should respond with JSON object with properties "activities" and "user" if username and password are found in the database', function(done) {
+
+    });
+  });
+
+  describe('GET /userinfo/:username', function() {
+    it('should respond with status 500 and no data if the username does not match a database record', function(done) {
+
+    });
+
+    it('should respond with a JSON object with properties "username", "profilepic", and "bio" if the username matches a database record', function(done) {
+
+    });
+  });
+});
